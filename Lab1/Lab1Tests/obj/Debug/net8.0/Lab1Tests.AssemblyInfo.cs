186aa48e8f42d1ce53c090cc60fccc0af8ddd475
YuraKodenov <yura_kodenov@knu.ua>
2024-10-11 22:37:30 +0300

    made test build & run

--- Lab1/Lab1Tests/obj/Debug/net8.0/Lab1Tests.AssemblyInfo.cs
+++ Lab1/Lab1Tests/obj/Debug/net8.0/Lab1Tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cfa71c87967763a7e390614217bac5f8190b51")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab1Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab1Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
