f1697b4350b8a5b4e955f6709e73e90b1d1bab6a
YuraKodenov <yura_kodenov@knu.ua>
2024-10-11 23:49:55 +0300

    add tests3

--- Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a49c7b1f6154c3a3b53d756d23ff734c78aeb0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06aebefe0280693eb34aa5ecd70e30c26a34d3e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
